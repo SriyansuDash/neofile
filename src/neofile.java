de53820c1dc2143b3c045132600466bc0bcfac0d
SriyansuDash <sriyansudash@outlook.com>
2023-10-03 13:02:56 +0530

    HTML Code

--- src/neofile.java
+++ src/neofile.java
@@ -5256,39 +5256,108 @@
 // }
 
 
+
+// import java.util.*;
+// public class neofile{
+//     static class Queue{
+//         static int arr[];
+//         static int size ;
+//         static int rear =-1;
+//         static int front =-1;
+//         Queue(int length){
+//             this.size = length;
+//             arr = new int[length];
+//         }
+//         public static boolean isEmpty(){
+//             return rear == -1 ? true : false; 
+//         }
+//         public static boolean isFull(){
+//             return (rear+1)%size ==-1 ? true : false;
+//         }
+//         public static void add(int data){
+//             if(isFull()){
+//                 System.out.println("\nOverflow\n");
+//                 return ;
+//             }
+//             if(front == -1){
+//                 front = 0;
+//             }
+//             rear = (rear+1)%size ;
+//             arr[rear] = data;
+//         }
+//         public static int remove(){
+//             if(isEmpty()){
+//                 System.out.println("Empty");
+//                 return -1;
+//             }
+//             int result = front;
+//             if(rear == front){
+//                 rear = front=-1;
+//             }
+//             else{
+//                 front = (front+1)%size;
+//             }
+//             return result;
+//         }
+//         public static int display(){
+//             if(isEmpty()){
+//                 System.out.println("Queue is empty");
+//                 return -1;
+//             }
+//             return arr[front];
+//         }
+//     }
+//     public static void main(String[] args) {
+//         Queue q = new Queue(Short.MAX_VALUE);
+//         q.add(0);
+//         q.add(1);
+//         q.add(2);
+//         q.add(3);
+//         q.add(4);
+//         q.add(5);
+//         q.add(6);
+//         System.out.println(q.remove());
+//         q.add(7);
+//         System.out.println(q.remove());
+//         q.add(8);
+//         System.out.println(q.remove());
+//         q.add(9);
+//         while(!q.isEmpty()){
+//             System.out.println(q.display());
+//             q.remove();
+//         }
+//     }
+// }
+
+// public class neofile{
+//     public static void print(int n){
+//         for(int i=0;i<n;i++){
+//             System.out.println("i \s"+i);
+//         }
+//         for(int j=0;j<n;j++){
+//             System.out.println("j \s"+j);
+//         }
+//     }
+// }
+
 import java.util.*;
 public class neofile{
-    static class Queue{
-        static int arr[];
-        static int size ;
-        static int rear =-1;
-        static int front =-1;
-        Queue(int length){
-            arr = new int[length];
-            this.size = length;
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        System.out.println("Enter the size of Array : ");
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        // input Array 
+        for (int i=0;i<n;i++){
+            arr[i] =  in.nextInt();
         }
-        public static boolean isEmpty(){
-            return (front == -1 && rear==-1)?true:false;
+        System.out.println("enter k postion : ");
+        int k = in.nextInt();
+        for(int i=k;i<n;i++){
+            System.out.println(arr[i]);
         }
-        public static boolean isFull(){
-            return (rear + 1)%size == front ? true : false;
-        }
-        public static void add(int data){
-            if(isFull()){
-                System.out.println("\nOverflow\n");
-                return;
-            }
-            if(front == -1){
-                front = 0;
-            }
-            rear = (rear+1)%size;
-            arr[rear] = data;
-        }
-        public static int remove(){
-            if(isEmpty()){
-                System.out.println("Queue is empty");
-            }
-
+        for(int i=0;i<k;i++){
+            System.out.println(arr[i]);
         }
     }
 }
